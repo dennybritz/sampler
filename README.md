f8ef4392a81402c0a60b6a113b98763b8266f9c1
Denny Britz <dennybritz@gmail.com>
2014-01-19 19:54:33 -0800

    Sampler usage and readme.

---
+++ README.md
@@ -0,0 +1,40 @@
+Gibbs sampler for factor graphs, including weight learning.
+
+Usage:
+
+```
+Usage: sampler [options]
+
+  -w <weightsFile> | --weights <weightsFile>
+        weights file in TSV format (required)
+  -v <variablesFile> | --variables <variablesFile>
+        variables file in TSV format (required)
+  -f <factorsFile> | --factors <factorsFile>
+        factors File in TSV format (required)
+  -o <outputFile> | --outputFile <outputFile>
+        output file path (required)
+  -i <numSamplesInference> | --numSamplesInference <numSamplesInference>
+        number of samples during inference (default: 100)
+  -l <learningNumIterations> | --learningNumIterations <learningNumIterations>
+        number of iterations during weight learning (default: 100)
+  -s <learningNumSamples> | --learningNumSamples <learningNumSamples>
+        number of samples per iteration during weight learning (default: 1)
+  --alpha <learningRate>
+        the learning rate for gradient descent (default: 0.1)
+  --diminish <diminishRate>
+        the diminish rate for learning (default: 0.95)
+  -t <numThreads> | --threads <numThreads>
+        This setting is no longer supported and will be ignored. The number of threads is automatically decided by the JVM.
+```
+
+Input format:
+
+```
+TODO
+```
+
+Output format:
+
+```
+TODO
+```
--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -8,30 +8,31 @@ case class Config(weightsFile: File, factorsFile: File, variablesFile: File, out
 
 object Runner extends App with Logging {
 
-  val parser = new scopt.OptionParser[Config]("scopt") {
+  val parser = new scopt.OptionParser[Config]("sampler") {
     opt[File]('w', "weights") required() valueName("<weightsFile>") action { (x, c) =>
-      c.copy(weightsFile = x) } text("weights File")
+      c.copy(weightsFile = x) } text("weights file in TSV format (required)")
     opt[File]('v', "variables") required() valueName("<variablesFile>") action { (x, c) =>
-      c.copy(variablesFile = x) } text("variables File")
+      c.copy(variablesFile = x) } text("variables file in TSV format (required)")
     opt[File]('f', "factors") required() valueName("<factorsFile>") action { (x, c) =>
-      c.copy(factorsFile = x) } text("factors File")
+      c.copy(factorsFile = x) } text("factors File in TSV format (required)")
     opt[File]('o', "outputFile") required() valueName("<outputFile>") action { (x, c) =>
-      c.copy(outputFile = x) } text("output file")
-    opt[Int]('i', "numSamplesInference") required() valueName("<numSamplesInference>") action { (x, c) =>
-      c.copy(numSamplesInference = x) } text("number of samples for inference")
+      c.copy(outputFile = x) } text("output file path (required)")
+    opt[Int]('i', "numSamplesInference") valueName("<numSamplesInference>") action { (x, c) =>
+      c.copy(numSamplesInference = x) } text("number of samples during inference (default: 100)")
     opt[Int]('l', "learningNumIterations") valueName("<learningNumIterations>") action { (x, c) =>
-      c.copy(learningNumIterations = x) } text("number of iterations during weight learning")
+      c.copy(learningNumIterations = x) } text("number of iterations during weight learning (default: 100)")
     opt[Int]('s', "learningNumSamples") valueName("<learningNumSamples>") action { (x, c) =>
-      c.copy(learningNumSamples = x) } text("number of samples per iteration during weight learning")
+      c.copy(learningNumSamples = x) } text("number of samples per iteration during weight learning (default: 1)")
     opt[Double]("alpha") valueName("<learningRate>") action { (x, c) =>
       c.copy(learningRate = x) } text("the learning rate for gradient descent (default: 0.1)")
-    opt[Double]("diminish") valueName("<learningRate>") action { (x, c) =>
+    opt[Double]("diminish") valueName("<diminishRate>") action { (x, c) =>
       c.copy(diminishRate = x) } text("the diminish rate for learning (default: 0.95)")
     opt[Int]('t',"threads") valueName("<numThreads>") action { (x, c) =>
       log.warn("Setting the number of threads is no longer supported. " + 
         "It's automatically decided by the JVM. Setting ignored.")
       c
-    } text ("number of threads.")
+    } text ("This setting is no longer supported and will be ignored. " +
+      "The number of threads is automatically decided by the JVM.")
   }
 
   val config = parser.parse(args, Config(null, null, null, null, 100, 100, 1, 0.1, 0.95)).getOrElse{
