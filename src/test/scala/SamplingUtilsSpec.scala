8976c5ce73b1b5b32071064a7870f0b6df4144ad
Denny Britz <dennybritz@gmail.com>
2014-01-21 20:24:22 -0800

    Added test for sampling a negated variable.

--- src/test/scala/SamplingUtilsSpec.scala
+++ src/test/scala/SamplingUtilsSpec.scala
@@ -13,11 +13,13 @@ class SamplingUtilsSpec extends FunSpec {
     Map(
       0 -> BooleanVariable(0, 0.0, false, true),
       1 -> BooleanVariable(1, 0.0, false, true),
-      2 -> BooleanVariable(2, 1.0, true, false)
+      2 -> BooleanVariable(2, 1.0, true, false),
+      3 -> BooleanVariable(3, 0.0, false, true)
     ),
     Map(
       0 -> Factor(0, List(FactorVariable(0, true)), 0, ImplyFactorFunction),
-      1 -> Factor(1, List(FactorVariable(1, true), FactorVariable(2, true)), 1, ImplyFactorFunction)
+      1 -> Factor(1, List(FactorVariable(1, true), FactorVariable(2, true)), 1, ImplyFactorFunction),
+      2 -> Factor(2, List(FactorVariable(3, false)), 0, ImplyFactorFunction)
     )
   )
 
@@ -29,6 +31,13 @@ class SamplingUtilsSpec extends FunSpec {
       assert(graphContext.getVariableValue(0) == 1.0)
       SamplingUtils.sampleVariable(1)
       assert(graphContext.getVariableValue(1) == 0.0)
+    }
+  }
+
+  describe("Sampling a variable that is negated in the factor funtion") {
+    it("should work") {
+      SamplingUtils.sampleVariable(3)
+      assert(graphContext.getVariableValue(3) == 0.0)
     }
   }
 
